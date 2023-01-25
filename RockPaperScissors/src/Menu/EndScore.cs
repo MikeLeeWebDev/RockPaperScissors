411b68d534278fa5a17dfc82165a93234391348c
fenetreVRT <fenetreverte@outlook.com>
2023-01-25 14:18:25 +0100

    Third pair coding: Start refactoring

---
+++ RockPaperScissors/src/Menu/EndScore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.src.Menu
+{
+    internal class EndScore
+    {
+    }
+}
---
+++ RockPaperScissors/src/Players/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.src.Players
+{
+    internal class Player
+    {
+
+        private string _playerName;
+        public string PlayerName
+        {
+            get
+            {
+                return _playerName;
+            }
+            set
+            {
+                _playerName = value;
+            }
+        }
+
+        public Player(string? playerName)
+        {
+            PlayerName = playerName;
+        }
+
+
+
+    }
+}
