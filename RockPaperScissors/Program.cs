f55806ad4dd5c8601c01f6226839676c203d8b6f
Micke Ly <mickelywebdev@gmail.com>
2023-02-05 18:40:41 +0100

    CLeaned a few lines

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -5,15 +5,6 @@ using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 
-
-//Random random = new Random();
-
-
-// Menu Variabler
-//(int left, int top) = Console.GetCursorPosition();
-//
-
-
 Console.WriteLine("Enter your name: ");
 Variables.yourName = Console.ReadLine();
 Console.CursorVisible = false;
